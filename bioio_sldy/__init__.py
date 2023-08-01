b3a77149ade4ae0558ae7b639f850dc357eb58b7
sean meharry <seanme97@gmail.com>
2023-08-01 07:49:30 -0700

    Init from cookiecutter

---
+++ bioio_sldy/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bioio_sldy."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("bioio-sldy")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+__author__ = "Sean Meharry"
+__email__ = "seanm@alleninstitute.org"
+
+
+from .reader_metadata import ReaderMetadata
+
+__all__ = ["ReaderMetadata"]
